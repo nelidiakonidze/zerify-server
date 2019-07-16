241302f6101bf43717f3c8bb61ca4c88bc9f5e2c
Neli <diakonidzeneli@gmail.com>
2019-07-16 13:32:04 +0200

    fixing bug part4

--- server/migrations/20190716104041-create-certificate.js
+++ server/migrations/20190716104041-create-certificate.js
@@ -1,30 +1,30 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Certificates', {
+    return queryInterface.createTable('Certificate', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       hash: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       settingscertificate: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Certificates');
-  }
+    return queryInterface.dropTable('Certificate');
+  },
 };
