50e26a4a59c897423e4fdd212a28ed14ed420b38
Neli <diakonidzeneli@gmail.com>
2019-07-24 16:19:45 +0200

    changed findOne to findAll and used include after where

--- server/controllers/certificates.js
+++ server/controllers/certificates.js
@@ -7,24 +7,24 @@ const create = async (req, res) => {
   //   return res.status(400).send({message: 'Invalid hash'});
   // }
 
-  if (!settings) {
-    return res.status(400).send({message: 'Invalid settings'});
-  }
+  var hash = uuidv4();
+  console.log(hash);
+
+  // if (settings) {
+  //   return res.hash.send({message: 'you have your hash'});
+  // }
 
   const certificate = await Certificates.create({
     hash,
     settings,
   });
-  return res.status(200).send({certificate});
+  return res.status(hash).send({certificate});
 };
 const list = async (req, res) => {
   const certificate = await Certificates.findAll({});
 
   return res.status(200).send({certificate});
 };
-
-var hashed = uuidv4();
-console.log(hashed);
 
 const getCertificateByHash = async (req, res) => {
   const {hash} = req.params;
@@ -51,41 +51,41 @@ const getCertificateByHash = async (req, res) => {
     return res.sendStatus(400);
   }
 };
-const email = async (req, res) => {
-  //hash code
-  //insert to data base
-  //config email
-  let transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: 'godfrey.ledner71@ethereal.email', // generated ethereal user
-      pass: 'BYpVyN6qEx3F7aVUDV', // generated ethereal password
-    },
-  });
+// const email = async (req, res) => {
+//   //hash code
+//   //insert to data base
+//   //config email
+//   let transporter = nodemailer.createTransport({
+//     host: 'smtp.ethereal.email',
+//     port: 587,
+//     secure: false, // true for 465, false for other ports
+//     auth: {
+//       user: 'godfrey.ledner71@ethereal.email', // generated ethereal user
+//       pass: 'BYpVyN6qEx3F7aVUDV', // generated ethereal password
+//     },
+//   });
 
-  let mailOptions = {
-    from: '"Team Zertify" <godfrey.ledner71@ethereal.email>', // sender address
-    to: 'tingel.tangel7@yahoo.de', // list of receivers
-    subject: 'Zertify Certificate Notification', // Subject line
-    text: `Congratulations, click the link to open your certificate: "EXAMPLE"`, // plain text body
-    html: '<b>Hi there here will be your certificate link in the future!</b>', // html body
-  };
-  // build template email
-  //create link with certificate
-  // sent email
+//   let mailOptions = {
+//     from: '"Team Zertify" <godfrey.ledner71@ethereal.email>', // sender address
+//     to: 'tingel.tangel7@yahoo.de', // list of receivers
+//     subject: 'Zertify Certificate Notification', // Subject line
+//     text: `Congratulations, click the link to open your certificate: "EXAMPLE"`, // plain text body
+//     html: '<b>Hi there here will be your certificate link in the future!</b>', // html body
+//   };
+//   // build template email
+//   //create link with certificate
+//   // sent email
 
-  // send mail with defined transport object
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      return console.log(error);
-    }
-    console.log('Message sent: %s', info.messageId);
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-  });
-  email().catch(console.error);
-};
+//   // send mail with defined transport object
+//   transporter.sendMail(mailOptions, (error, info) => {
+//     if (error) {
+//       return console.log(error);
+//     }
+//     console.log('Message sent: %s', info.messageId);
+//     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+//   });
+//   email().catch(console.error);
+// };
 
 module.exports = {
   create,
