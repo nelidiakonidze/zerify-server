bae7a8f87a45509c7745a20fb61c6ce9b49f60d3
Neli <diakonidzeneli@gmail.com>
2019-07-25 15:57:56 +0200

    added nodemailer

--- server/controllers/certificates.js
+++ server/controllers/certificates.js
@@ -22,7 +22,7 @@ const create = async (req, res) => {
   console.log('this is the', res);
 
   // Call our email function to send the email with the generated hash.
-  // await email(hashed);
+  await email(hashed);
 
   return res.send({hashed});
   // .catch(email)
@@ -58,6 +58,46 @@ const getCertificateByHash = async (req, res) => {
     console.log({err});
     return res.sendStatus(400);
   }
+};
+
+const email = async hashed => {
+  //config email
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: 'lance.morar98@ethereal.email', // generated ethereal user
+      pass: 'G2x1G7JKQ26b2Xrq53', // generated ethereal password
+    },
+  });
+
+  // const baseUrl = 'https://zertify.netlify.com';
+  const baseUrl = 'http://localhost:3000/';
+
+  const urlLink = `${baseUrl}/certificate/sent/${hashed}`;
+  const email = 'camille.charteau@hotmail.fr';
+
+  let mailOptions = {
+    from: '"Team Zertify" <lance.morar98@ethereal.email>', // sender address
+    to: `${email}`, // list of receivers
+    subject: 'Zertify Certificate Notification', // Subject line
+    text: `Congratulations, click the link to open your certificate: "EXAMPLE"`, // plain text body
+    html: `<b><a href="${urlLink}" >Go to certificate</a></b>`, // html body
+  };
+  // build template email
+  //create link with certificate
+  // sent email
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message sent: %s', info.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  });
+  // email().catch(console.error);
 };
 
 module.exports = {
