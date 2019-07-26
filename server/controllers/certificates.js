870b322d2e2696fb50110a56066f8e76f75781c2
Neli <diakonidzeneli@gmail.com>
2019-07-26 11:37:26 +0200

    added giphy

--- server/controllers/certificates.js
+++ server/controllers/certificates.js
@@ -102,7 +102,6 @@ const email = async (hashed, settings) => {
     Below you can find the link to download and print the certificate. 
     <h8>
     <h12>*Please, note that the link is only available in Desktop devices and Apple smartphones. </h12>
-    <div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe src="https://giphy.com/embed/LM9APuedFPR5YWSmhM" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/unibogazici-graduate-boun-bogazici-universitesi-LM9APuedFPR5YWSmhM">via GIPHY</a></p>
     <a href="${urlLink}">Go to certificate</a></b>`, // html body
   };
   // build template email
